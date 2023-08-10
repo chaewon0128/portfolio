6688f4b71ab0a2f34d40422c2ebd56678c3faeec
chaewon <juh7814@gmail.com>
2023-08-10 20:55:45 +0900

    🐛  tailwind 동적 스타일링 오류 해결

--- components/layout/TerminalLayout.tsx
+++ components/layout/TerminalLayout.tsx
@@ -2,23 +2,30 @@ import { useRouter } from "next/router";
 import Ball from "../ball/Ball";
 import { ReactNode } from "react";
 import { motion } from "framer-motion"
-
-
 interface ITerminal {
     children: ReactNode,
     isClose?: boolean,
-    width?: string,
-    height?: string,
+    width: string
 }
-export default function TerminalLayout({ children, isClose = false, width = "700px", height = "400px" }: ITerminal) {
+
+interface SizeType {
+    [propName: string]: string;
+}
+
+export default function TerminalLayout({ children, isClose = false, width }: ITerminal) {
     const router = useRouter();
+    const size: SizeType = {
+        xl: 'w-[700px] h-[400px]',
+        md: 'w-[700px] h-[400px]',
+    }
+
 
     return (
         <>
             <motion.div initial={{ scaleX: isClose ? 1 : 0 }} animate={{ scaleX: isClose ? 0 : 1 }} transition={{
                 type: "spring",
                 restDelta: 0.5
-            }} className={`bg-[#3B3B3B] shadow-black shadow-2xl  w-[${width}] h-[${height}] `} >
+            }} className={`bg-[#3B3B3B] shadow-black shadow-2xl  ${size[width]} `} >
                 <div className='pl-2 space-x-1 bg-slate-50' >
                     <Ball color="red" />
                     <Ball color="yellow" />
