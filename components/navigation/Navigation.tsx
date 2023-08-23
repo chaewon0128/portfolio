8d456650892e1bc22bba816dbff1d3b46d5a60c3
chaewon <juh7814@gmail.com>
2023-08-23 13:38:43 +0900

    💄 페이지 사이즈에 따라 텍스트 사이즈 변경

--- components/navigation/Navigation.tsx
+++ components/navigation/Navigation.tsx
@@ -6,15 +6,17 @@ export default function Navigation() {
     return (
 
 
-        <nav className="bg-transparent fixed top-0 w-full text-white pt-2">
+        <nav className="bg-transparent fixed top-0 w-full text-white pt-2 z-40">
             <div className="flex justify-between items-center h-10 px-5">
-                <div className="flex items-center w-32 justify-between">
-                    <div className="w-4">
+                <div className="flex items-center w-[180px] ">
+                    <div className="w-4 animate-bounce mr-3">
                         <HeartIcon />
                     </div>
-                    <h1 className="font-press-start flex justify-center items-center text-xs">CHAE WON</h1>
+                    <h1 className="font-press-start flex justify-center items-center text-xs md:text-lg">CHAE WON</h1>
                 </div>
-                <ul>
+                <ul className="flex justify-between w-60 md:text-lg">
+                    <li className="font-thin">about</li>
+                    <li className="font-thin">work</li>
                     <li className="font-thin">contact</li>
                 </ul>
             </div>
