d43804c3b7b088eb4835ea22e57a823f7a404f39
chaewon <juh7814@gmail.com>
2023-09-01 12:02:21 +0900

    💄 텍스트 위치 조절

--- components/landing/Landing.tsx
+++ components/landing/Landing.tsx
@@ -12,13 +12,13 @@ export default function Landing({ mainRef }: ILanding) {
                 <div className='hidden md:block w-full h-screen '>
                     <video src={require('../../public/assets/whale.mp4')} loop={true} autoPlay={true} className="h-[100%] w-full object-fill" />
                 </div>
-                <div className='absolute md:top-[50%] text-center md:text-left md:pl-10 w-full'>
+                <div className='absolute md:bottom-[10%] text-center md:text-left md:pl-10 w-full'>
                     <h2 className='title ml-2'><span className='accent-text'>C</span>reative</h2>
                     <h3 className='title'>Developer</h3>
                     <p className='title text-lg pl-[90px] md:text-xl md:mt-5 lg:text-2xl lg:mt-[30px]'>no lisk no fun</p>
                 </div>
-                <div className='w-full h-[150px] absolute bottom-0 text-center md:w-[400px] md:right-0 md:bottom-[30%]'>
-                    <p className='title font-Bebas-neue text-2xl w-[400px] m-auto  md:text-5xl md:w-[400px] '> INTERACTIVE WEB <span className='accent-text'>DEVELOPMENT</span> ACCESSIBILITY <span className='accent-text'>YOUTUBE</span> BASEBALL<span className='accent-text'> WORKOUT</span> HEALTHY LIFE <span className='accent-text'>TRAVEL</span> POSITIVE <span className='accent-text'>WHALE</span></p>
+                <div className='w-full h-[150px] absolute bottom-0 text-center md:w-[400px] md:right-[3%] md:bottom-[50%] lg:top-[20%]'>
+                    <p className='title font-Bebas-neue text-2xl w-[400px] m-auto  md:text-5xl md:w-[400px] lg:text-6xl '> INTERACTIVE WEB <span className='accent-text'>DEVELOPMENT</span> ACCESSIBILITY <span className='accent-text'>YOUTUBE</span> BASEBALL<span className='accent-text'> WORKOUT</span> HEALTHY LIFE <span className='accent-text'>TRAVEL</span> POSITIVE <span className='accent-text'>WHALE</span></p>
                 </div>
             </div>
 
