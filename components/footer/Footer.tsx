f66713114f4e9dca17bf0eba123925785173f8e6
chaewon <juh7814@gmail.com>
2023-09-07 21:31:16 +0900

    💄 footer height 수정

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -13,7 +13,7 @@ export default function Footer({ onNavigate }: IFooter) {
     const onHideImg = () => setShowImg(false);
 
     return (
-        <footer className='h-[699px] mt-10 relative font-Bebas-neue text-white lg:h-[100px] lg:bg-black lg:flex lg:items-center' id="contact" onMouseEnter={onShowImg} onMouseLeave={onHideImg}>
+        <footer className='h-[899px] mt-10 relative font-Bebas-neue text-white lg:h-[100px] lg:bg-black lg:flex lg:items-center' id="contact" onMouseEnter={onShowImg} onMouseLeave={onHideImg}>
             <h2 className='text-5xl items-start pt-[100px] text-center lg:pt-0  lg:text-left lg:pl-20' >Contact Me</h2>
             {showImg ? <Contact /> : null}
             <ul className='flex flex-col justify-around items-center w-full h-[200px] text-2xl mt-10 lg:flex-row lg:h-0 lg:w-[500px] lg:mt-0' >
