ef8521108327a5839ae7394767c8427defbddb99
chaewon <juh7814@gmail.com>
2023-09-07 21:30:17 +0900

    💄 모바일 버전 사이즈와 위치 수정

--- components/contact/Contact.tsx
+++ components/contact/Contact.tsx
@@ -3,7 +3,7 @@ import contactMe from '../../public/assets/contact.png'
 
 export default function Contact() {
     return (
-        <div className='bg-transparent w-40  h-40 absolute top-[-65px] left-[40%] md:top-[-140px] md:left-[100px]'>
+        <div className='bg-transparent w-96  h-96  absolute bottom-16 left-[10%] md:w-40 md:h-40 md:top-[-140px] md:left-[100px]'>
             <Image src={contactMe} layout='fill' alt='프로필 미모티콘' />
         </div>
     )
