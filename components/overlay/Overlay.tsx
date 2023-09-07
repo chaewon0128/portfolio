3a43d474fa96fbc18e567049d03896d514b2cd5b
chaewon <juh7814@gmail.com>
2023-09-07 21:32:08 +0900

    ⚡️ 동적으로 배경 색상 변경

--- components/overlay/Overlay.tsx
+++ components/overlay/Overlay.tsx
@@ -1,35 +1,51 @@
 import { useEffect, useRef } from 'react'
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
-export default function Overlay() {
+import MySkill from '../mySkill/MySkill';
+import AboutMe from '../aboutMe/AboutMe';
+
+interface IOverlay {
+    color: string,
+}
+
+interface ColorMap {
+    [key: string]: string;
+}
+
+export default function Overlay({ color }: IOverlay) {
     const changeRef = useRef<HTMLDivElement>(null)
+    const colorVariants: ColorMap = {
+        blue: 'bg-[#114863]',
+        black: 'bg-black bg-opacity-70',
+    }
+
     gsap.registerPlugin(ScrollTrigger);
     useEffect(() => {
         const tl = gsap.timeline({
             scrollTrigger: {
                 trigger: changeRef.current,
-                start: 'top top',
+                start: 'top -5%',
                 end: 'top top',
             },
         })
         let ctx = gsap.context(() => {
-            tl.to('.bar', { height: 0, ease: "power4.inOut", stagger: { amount: 0.5 } })
+            tl.to('.bar', { height: 0, ease: "power4.inOut", duration: 1, stagger: { amount: 0.5 } })
         }, changeRef);
-
         return () => ctx.revert();
     }, [])
     return (
-        <div className='w-full h-[100vh] flex z-50' ref={changeRef} >
-            <div className='bar w-[10vw] h-[105vh] bg-slate-600'></div>
-            <div className='bar w-[10vw] h-[105vh] bg-slate-600'></div>
-            <div className='bar w-[10vw] h-[105vh] bg-slate-600'></div>
-            <div className='bar w-[10vw] h-[105vh] bg-slate-600'></div>
-            <div className='bar w-[10vw] h-[105vh] bg-slate-600'></div>
-            <div className='bar w-[10vw] h-[105vh] bg-slate-600'></div>
-            <div className='bar w-[10vw] h-[105vh] bg-slate-600'></div>
-            <div className='bar w-[10vw] h-[105vh] bg-slate-600'></div>
-            <div className='bar w-[10vw] h-[105vh] bg-slate-600'></div>
-            <div className='bar w-[10vw] h-[105vh] bg-slate-600'></div>
+        <div className='w-full h-[100vh] flex z-50 ' ref={changeRef} >
+            <div className={`bar w-[10vw] h-[105vh] ${colorVariants[color]}`}></div>
+            <div className={`bar w-[10vw] h-[105vh] ${colorVariants[color]}`}></div>
+            <div className={`bar w-[10vw] h-[105vh] ${colorVariants[color]}`}></div>
+            <div className={`bar w-[10vw] h-[105vh] ${colorVariants[color]}`}></div>
+            <div className={`bar w-[10vw] h-[105vh] ${colorVariants[color]}`}></div>
+            <div className={`bar w-[10vw] h-[105vh] ${colorVariants[color]}`}></div>
+            <div className={`bar w-[10vw] h-[105vh] ${colorVariants[color]}`}></div>
+            <div className={`bar w-[10vw] h-[105vh] ${colorVariants[color]}`}></div>
+            <div className={`bar w-[10vw] h-[105vh] ${colorVariants[color]}`}></div>
+            <div className={`bar w-[10vw] h-[105vh] ${colorVariants[color]}`}></div>
+
         </div>
     )
 }
