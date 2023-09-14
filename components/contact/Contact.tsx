f9f993208ff0c95813a60b3f9fbb86ffa9d77619
chaewon <juh7814@gmail.com>
2023-09-14 14:38:43 +0900

    📱 : "반응형에 따른 이미지 위치 수정"

--- components/contact/Contact.tsx
+++ components/contact/Contact.tsx
@@ -3,7 +3,7 @@ import contactMe from '../../public/assets/contact.png'
 
 export default function Contact() {
     return (
-        <div className='bg-transparent w-96  h-96  absolute bottom-16 left-[10%] md:w-40 md:h-40 md:top-[-140px] md:left-[100px]'>
+        <div className='bg-transparent w-96 h-96  absolute bottom-16 left-[10%] md:left-[25%] lg:w-40 lg:h-40 lg:top-[-140px] lg:left-[100px]'>
             <Image src={contactMe} layout='fill' alt='프로필 미모티콘' />
         </div>
     )
