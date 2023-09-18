5e5a32a340f317491e51bd24bf05631dca56c9c0
chaewon <juh7814@gmail.com>
2023-09-18 13:40:44 +0900

    ⚡️ : "아이콘 추가"

--- components/landing/Landing.tsx
+++ components/landing/Landing.tsx
@@ -1,5 +1,6 @@
 import { gsap } from "gsap";
 import { useLayoutEffect, useRef } from "react"
+import { GiSaveArrow } from 'react-icons/gi';
 
 export default function LandingPart() {
     const landingRef = useRef<HTMLDivElement>(null)
@@ -8,6 +9,7 @@ export default function LandingPart() {
         let ctx = gsap.context(() => {
             tl.from(".title", { opacity: 0, y: 100, stagger: 0.5, duration: 2 })
                 .to(".accent-text", { opacity: 0.3, stagger: 0.3 })
+                .from(".scroll", { opacity: 0 }, 2)
         }, landingRef);
 
         return () => ctx.kill();
@@ -22,10 +24,12 @@ export default function LandingPart() {
                     <h2 className='title ml-2'><span className='accent-text'>C</span>reative <br /><span className="title">Developer</span></h2>
                     <p className='title text-lg pl-[90px] md:text-xl mt-5 lg:text-2xl lg:mt-[30px]'>no lisk no fun</p>
                 </div>
-                <div className='w-full h-[150px] absolute bottom-0 text-center md:w-[400px] md:right-[3%] md:bottom-[50%] lg:top-[20%]'>
+                <div className='w-full h-[150px] absolute bottom-10 text-center md:w-[400px] md:right-[3%] md:bottom-[50%] lg:top-[20%]'>
                     <p className='title font-Bebas-neue text-2xl w-[400px] m-auto  md:text-5xl md:w-[400px] lg:text-6xl '> INTERACTIVE WEB <span className='accent-text'>DEVELOPMENT</span> ACCESSIBILITY <span className='accent-text'>YOUTUBE</span> BASEBALL<span className='accent-text'> WORKOUT</span> HEALTHY LIFE <span className='accent-text'>TRAVEL</span> POSITIVE <span className='accent-text'>WHALE</span></p>
                 </div>
             </div>
+            <div className="scroll absolute bottom-5 left-[50%] animate-bounce">
+                <GiSaveArrow size={20} /></div>
 
         </main>
     )
