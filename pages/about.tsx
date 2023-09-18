827cf16bb850dcc60b3aa7b91bdda903f5aa4fbc
chaewon <juh7814@gmail.com>
2023-09-18 13:41:21 +0900

    🧑‍💻 컴포넌트 분리

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,21 +1,22 @@
 import Layout from '@/components/layout/Layout';
-import SmallTerminal from '@/components/smallTerminal/SmallTerminal';
-import webCafe from '../assets/webcafe.png'
-import { motion, useMotionValueEvent, useScroll, useTransform } from 'framer-motion'
-import { useContext, useEffect, useRef, useState } from 'react';
-import Scroll from '@/components/scroll/ScrollToRight';
+import { useEffect, useRef, useState } from 'react';
 import Loading from '@/components/loading/Loading';
 import Navigation from '@/components/navigation/Navigation';
-import AboutMe from '@/components/aboutme/AboutMe';
-import ScrollToRight from '@/components/scroll/ScrollToRight';
-import ScrollToLeft from '@/components/scroll/ScrollToLeft';
-import { PageContext } from '@/store/page';
+import Footer from '@/components/footer/Footer';
+import LandingPart from '@/components/landing/Landing';
+import AboutMe from '@/components/aboutMe/AboutMe';
+import Project from '@/components/project/Project';
+import Overlay from '@/components/overlay/Overlay';
+import MySkill from '@/components/mySkill/MySkill';
+
 
 export default function About() {
     const [isLoading, setIsLoading] = useState(true)
-    const { scrollYProgress } = useScroll();
-    const pageData = useContext(PageContext)
 
+    // gsap.config({ nullTargetWarn: false })
+    const onNavigate = (url: string) => {
+        window.open(url, '_blank');
+    }
     useEffect(() => {
         const timer = setTimeout(() => {
             setIsLoading(false)
@@ -27,40 +28,23 @@ export default function About() {
     }, []);
 
 
-
     return (
-        <>
+        <Layout>
             {isLoading ? <Loading /> : (
                 <>
                     <Navigation />
-                    <AboutMe />
-                    <motion.div
-                        initial={{ opacity: 1, y: 100 }}
-                        whileInView={{
-                            y: 0,
-                            opacity: 1,
-                            backgroundColor: "black",
-                            transition: {
-                                type: "spring",
-                                bounce: 0.4,
-                                duration: 0.8
-                            }
-                        }}
-                        className='min-h-screen w-full'>
-                        <div className='text-transparent'>.</div>
-                        <ScrollToRight src={webCafe} alt="web cafe" url="https://chaewon0128.github.io/html5" introduction='웹표준을 준수하여 제작한 웹사이트 입니다' />
-                        <div className='text-transparent'>.</div>
-                    </motion.div>
-                    <motion.div className='bg-black min-h-screen w-full'>
-                        <div>dd</div>
-                        <ScrollToLeft src={webCafe} alt="web cafe" url="https://chaewon0128.github.io/html5" introduction='웹표준을 준수하여 제작한 웹사이트 입니다' />
-                        <div>dd</div>
-                    </motion.div>
+                    <LandingPart />
+                    <Overlay color='blue' />
+                    <AboutMe onNavigate={onNavigate} />
+                    <Overlay color='black' />
+                    <MySkill />
+                    <Project />
+                    <Footer onNavigate={onNavigate} />
 
                 </>
             )}
 
-        </>
+        </Layout>
     );
 }
 
