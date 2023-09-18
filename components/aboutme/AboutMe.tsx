affa05c6fe50c378fc2937b36d5dc2ad7a1d8b28
chaewon <juh7814@gmail.com>
2023-09-18 17:58:49 +0900

    💄 about me 컴포넌트 구조 변경

--- components/aboutme/AboutMe.tsx
+++ components/aboutme/AboutMe.tsx
@@ -1,64 +1,44 @@
 import Image from "next/image";
-import { useEffect, useRef } from "react";
-import profile from '../../public/assets/hello.png';
-import { SiVelog, SiGithub, SiInstagram, SiNotion } from 'react-icons/si';
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
-interface IAboutMe {
-    onNavigate: (url: string) => void
-}
+import { SiNextdotjs, SiJavascript, SiTypescript, SiCss3, SiHtml5, SiReact } from 'react-icons/si';
+import develper from '../../public/assets/developer.webp';
+import selfie from '../../public/assets/selfie.jpg';
 
-export default function AboutMe({ onNavigate }: IAboutMe) {
-    const introduceRef = useRef<HTMLDivElement>(null)
-    gsap.registerPlugin(ScrollTrigger);
-    useEffect(() => {
-        const tl = gsap.timeline({
-            scrollTrigger: {
-                trigger: introduceRef.current,
-                start: 'top 50%',
-                end: '+=700',
-                scrub: true,
-                markers: true,
-            },
-        })
-        let ctx = gsap.context(() => {
-            tl.from(".intro", { delay: 5, opacity: 0, x: -100, duration: 2, ease: 'bounce.out' })
-            tl.from(".duce", { opacity: 0, x: 100, duration: 2, ease: 'bounce.out' })
-            tl.from(".background", { opacity: 0, duration: 1.5, })
-            tl.from(".introduce", { opacity: 0, y: 50, duration: 1 })
 
-        }, introduceRef);
+export default function AboutMe() {
 
-        return () => ctx.revert();
-    }, [])
-
+    const onMoveContact = () => {
+        const contact = document.getElementById('contact')!;
+        contact.scrollIntoView({ behavior: 'smooth' });
+    }
     return (
-        <div id='about' className='font-Hahmlet text-white text-2xl min-h-screen pt-[300px] pb-[300px] relative' ref={introduceRef} >
-            <div className='min-h-screen flex flex-col items-center justify-center space-y-20 text-[140px] font-Bebas-neue' >
-                {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((_i) => (
-                    <div key={_i} className='md:hidden'>
-                        <span className='intro inline-block'>Intro</span>
-                        <span className='duce inline-block'>duce</span>
-                    </div>
-                ))}
-                <div className='hidden md:block'>
-                    <span className='intro inline-block   text-[200px] lg:text-[300px]'>Intro</span>
-                    <span className='duce inline-block  text-[200px] lg:text-[300px]'>duce</span>
+        <div className='relative w-full min-h-screen px-10 2xl:px-[300px]' id="about">
+            <h2 className='text-[3em] xl:text-[9em] font-be-vietnam text-white '><span className='inline-block bg-lime-300 text-black w-36 lg:w-40 xl:w-[450px] text-center'>  WHO </span> <br /> ARE YOU?</h2>
+            <button className='font-be-vietnam text-black bg-lime-300 px-3 py-2 xl:px-6 xl:py-4 xl:text-[20px] rounded-full absolute right-10 2xl:right-[300px] top-15 lg:top-24 hover:bg-slate-400 hover:text-white' onClick={onMoveContact}>Contact Me</button>
+            <section className='mt-10 text-white md:text-right'>
+                <h3 className='text-2xl font-be-vietnam md:text-5xl'>about me.</h3>
+                <p className='text-lg md:text-xl mt-2'>안녕하세요 웹 프론트엔드 개발자 함채원 입니다. <br />
+                    새로운 기술과 인터랙티브 웹에 대한 열정을 가지고 있습니다 <br />
+                    제 목표는 고객중심으로 사고하며 사용자 경험을 최적화하고, <br /> 이를 통해 비즈니스의 성장에 기여하는 개발자가 되는 것입니다.</p>
+            </section>
+            <section className='relative w-[500px] md:left-[0] md:bottom-[0px] xl:bottom-[250px]'>
+                <div className='relative w-[350px] h-[467px] mt-10 left-20'>
+                    <Image src={develper} alt='개발자 사진' fill />
                 </div>
-            </div>
-            <div className='background absolute top-0 bg-black w-full h-full opacity-70 ' />
-            <div className='introduce z-100 absolute top-[35%] flex flex-col items-center justify-center m-auto w-full'>
-
-                <p className='w-full  text-center ' >새로운 도전과 인터렉티브한 웹을 좋아합니다<br />
-                    <br />다양한 경험을 토대로 <br /> 저만의 아이디어를 실현할 수 있습니다.</p>
-                <Image src={profile} width={300} alt='프로필 미모티콘' />
-                <div className='flex gap-7 text-white mt-10'>
-                    <button className='hover:opacity-30 hover:transition-opacity' onClick={() => onNavigate('https://github.com/chaewon0128')}><SiGithub size={30} /></button>
-                    <button className='hover:opacity-30 hover:transition-opacity' onClick={() => onNavigate('https://url.kr/ci3tmd')}><SiNotion size={30} /></button>
-                    <button className='hover:opacity-30 hover:transition-opacity' onClick={() => onNavigate('https://velog.io/@dolfin')}><SiVelog size={30} /></button>
-                    <button className='hover:opacity-30 hover:transition-opacity' onClick={() => onNavigate('https://www.instagram.com/nweahc/')}><SiInstagram size={30} /></button>
+                <div className='absolute w-[350px] h-[467px] top-[12%] left-0'>
+                    <Image src={selfie} alt='셀카 사진' fill />
                 </div>
-            </div>
+            </section>
+            <section className='hidden md:block absolute top-[50%] xl:top-[60%] 2xl:right-[300px] right-10 text-white w-[400px] text-right'>
+                <h3 className='text-2xl font-be-vietnam md:text-5xl'>my skills.</h3>
+                <div className='w-[200px] flex gap-4 mt-10 text-[50px]'>
+                    <div className='skill text-orange-500'><SiHtml5 /> </div>
+                    <div className='skill text-blue-600'><SiCss3 /> </div>
+                    <div className='skill text-yellow-400'><SiJavascript /> </div>
+                    <div className='skill text-blue-400'><SiTypescript /> </div>
+                    <div className='skill text-blue-300'><SiReact /> </div>
+                    <div className='skill'><SiNextdotjs /> </div>
+                </div>
+            </section>
         </div>
     )
 }
