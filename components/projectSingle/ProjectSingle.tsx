e30410068b9a3069338cc7313cd435edd635e6e6
chaewon <juh7814@gmail.com>
2023-09-18 23:31:31 +0900

    💄 project 컴포넌트 구조 변경

--- components/projectSingle/ProjectSingle.tsx
+++ components/projectSingle/ProjectSingle.tsx
@@ -20,33 +20,23 @@ export default function ProjectSingle({ project }: ProjectSingleType) {
         window.open(url, '_blank');
     }
     return (
-        <div className="scroll-section h-[100vh] w-[100vw] flex justify-center items-center ">
-            <div className="w-[400px] md:w-[800px] lg:w-[1000px] bg-white h-[80%] rounded-2xl p-14 flex flex-col space-y-2 relative">
-                <div className="min-w-[280px] h-[280px] mx-auto lg:mx-0 lg:mt-12 md:w-[350px] md:h-[350px] lg:w-[400px] lg:h-[300px] relative ">
-                    <Image
-                        src={imageUrl}
-                        alt={`${project.name}의 이미지`}
-                        fill
-                    />
-                </div>
-                <h3 className="font-extrabold text-5xl text-center pt-2 lg:text-left md:text-[4em] lg:text-[5em] lg:absolute lg:left-15 lg:bottom-40">{project.name}</h3>
-                <div className="lg:absolute lg:bottom-[100px] z-10 pt-2">
-                    {project.steck?.map((item, i) => <div className={`inline-block border border-black text-black text-xs rounded-xl p-2 mr-2 mb-2 ${i >= 2 ? 'top-28' : ''}`} key={i}>{item}</div>)}
-                </div>
-                <div className="w-[300px] md:w-[700px] md:h-[280px] md:pt-[20px] lg:absolute lg:right-16 lg:top-[200px] lg:h-[500px] lg:pt-0 lg:w-[440px] lg:leading-10">
-                    <div className="w-full text-md md:text-xl lg:text-xl font-nanum">{project.introduction}</div>
-                    <div className="absolute bottom-10 right-20">
-                        <button className="border border-black rounded-md w-[100px] p-2 mr-2 uppercase font-semibold  hover:text-blue-400 hover:border-blue-400 hover:transition-all transition-all" onClick={() => onVisitSite(project.url)}>
-                            visit
-                        </button>
-                        <button className="border border-black  rounded-md w-[100px] p-2 bg-black text-white uppercase font-semibold  hover:text-blue-400 hover:transition-all transition-all" onClick={() => onVisitSite(project.github)}>
-                            github
-                        </button>
+        <div className="scroll-section h-[100vh] w-[100vw] flex flex-col justify-center items-center relative text-yellow-50  pt-28 lg:px-[200px] ">
+            <div className="relative w-[500px] h-96 bottom-[150px] lg:w-[700px] lg:h-[500px] ">
+                <Image src={imageUrl} alt={`${project.name}의 이미지`} fill />
+            </div>
+            <section className="w-[80%] h-[250px] xl:h-[330px] md:flex md:justify-center md:gap-10 lg:gap-10 xl:gap-[100px]">
+                <h3 className="inline mb-3 text-4xl font-be-vietnam md:text-7xl lg:text-8xl">{project.name}</h3>
+                <div className="mt-5 md:mt-0">
+                    <p className="text-xl leading-[30px] md:text-2xl w-[450px] md:w-[450px] md:leading-10 xl:leading-[50px]  xl:text-3xl lg:w-[600px]">{project.introduction}</p>
+                    <div className="z-10 pt-5">
+                        {project.steck?.map((item, i) => <div className={`inline-block bg-yellow-50 text-sm text-black font-bold p-2 mr-2 mb-2 ${i >= 2 ? 'top-28' : ''}`} key={i}>{item}</div>)}
+                    </div>
+                    <div className="mt-9 md:mt-2">
+                        <span className="mr-2 transition-all border-b-4 border-lime-300 text-lime-300 font-be-vietnam hover:text-black hover:bg-lime-300 md:text-xl lg:text-3xl"><a href={project.url}>visit</a></span>
+                        <span className="transition-all border-b-4 border-lime-300 text-lime-300 font-be-vietnam lg:text-3xl hover:text-black hover:bg-lime-300 md:text-xl"><a href={project.github}>git hub</a></span>
                     </div>
                 </div>
-
-
-            </div>
+            </section>
 
         </div >
     )
