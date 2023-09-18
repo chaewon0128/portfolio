8ad3f778aad78e3cab7d0d36fd5ca280e69b44af
chaewon <juh7814@gmail.com>
2023-09-18 13:41:56 +0900

    📱 반응형에 따른 스타일 수정

--- components/projectSingle/ProjectSingle.tsx
+++ components/projectSingle/ProjectSingle.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image"
 
 type ProjectSingleType = { project: IProject }
-type SteckType = string[]
+type ItemsType = string[]
 
 interface IProject {
     id: number,
@@ -10,7 +10,7 @@ interface IProject {
     image: string,
     introduction: string,
     url: string,
-    steck: SteckType
+    steck: ItemsType
 }
 
 
@@ -20,33 +20,33 @@ export default function ProjectSingle({ project }: ProjectSingleType) {
         window.open(url, '_blank');
     }
     return (
-        <div className="scroll-section h-[100vh] w-[100vw] flex justify-center items-center bg-black bg-opacity-70 ">
-
-            <div className="w-[80%] bg-white h-[80%] rounded-2xl p-16 flex flex-col space-y-5">
-                <Image
-                    src={imageUrl}
-                    alt={`${project.name}의 이미지`}
-                    width={500}
-                    height={300}
-                />
-
-                <h3 className="font-extrabold text-5xl text-center">{project.name}</h3>
-                <div className="bg-red-200 ">
-                    {project.steck?.map((item, i) => <span className="border border-black  text-black rounded-md p-2 mr-2" key={i}>{item}</span>)}
+        <div className="scroll-section h-[100vh] w-[100vw] flex justify-center items-center ">
+            <div className="w-[400px] md:w-[800px] lg:w-[1000px] bg-white h-[80%] rounded-2xl p-14 flex flex-col space-y-2 relative">
+                <div className="min-w-[280px] h-[280px] mx-auto lg:mx-0 lg:mt-12 md:w-[350px] md:h-[350px] lg:w-[400px] lg:h-[300px] relative ">
+                    <Image
+                        src={imageUrl}
+                        alt={`${project.name}의 이미지`}
+                        fill
+                    />
                 </div>
-                <p className="py-5">{project.introduction}</p>
-                <div className="text-right">
-                    <button className="border border-black rounded-md w-[100px] p-2 mr-2 uppercase font-semibold  hover:text-blue-300 hover:transition-all transition-all" onClick={() => onVisitSite(project.url)}>
-                        visit
-                    </button>
-                    <button className="border border-black  rounded-md w-[100px] p-2 bg-black text-white uppercase font-semibold  hover:text-blue-300 hover:transition-all transition-all" onClick={() => onVisitSite(project.github)}>
-                        github
-                    </button>
+                <h3 className="font-extrabold text-5xl text-center pt-2 lg:text-left md:text-[4em] lg:text-[5em] lg:absolute lg:left-15 lg:bottom-40">{project.name}</h3>
+                <div className="lg:absolute lg:bottom-[100px] z-10 pt-2">
+                    {project.steck?.map((item, i) => <div className={`inline-block border border-black text-black text-xs rounded-xl p-2 mr-2 mb-2 ${i >= 2 ? 'top-28' : ''}`} key={i}>{item}</div>)}
                 </div>
+                <div className="w-[300px] md:w-[700px] md:h-[280px] md:pt-[20px] lg:absolute lg:right-16 lg:top-[200px] lg:h-[500px] lg:pt-0 lg:w-[440px] lg:leading-10">
+                    <div className="w-full text-md md:text-xl lg:text-xl font-nanum">{project.introduction}</div>
+                    <div className="absolute bottom-10 right-20">
+                        <button className="border border-black rounded-md w-[100px] p-2 mr-2 uppercase font-semibold  hover:text-blue-400 hover:border-blue-400 hover:transition-all transition-all" onClick={() => onVisitSite(project.url)}>
+                            visit
+                        </button>
+                        <button className="border border-black  rounded-md w-[100px] p-2 bg-black text-white uppercase font-semibold  hover:text-blue-400 hover:transition-all transition-all" onClick={() => onVisitSite(project.github)}>
+                            github
+                        </button>
+                    </div>
+                </div>
+
+
             </div>
-
-
-
 
         </div >
     )
